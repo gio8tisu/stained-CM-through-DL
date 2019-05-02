fb1b6630aa4084ee6ab8ab4d2423be86ca333b35
gio8tisu <gio8.tisu@gmail.com>
2019-05-02 20:02:03 +0200

    basic despeckling model

--- despeckling/model.py
+++ despeckling/model.py
@@ -6,44 +6,63 @@ import torch
 
 import sys
 
-class ConvModel(nn.Module):
-    def __init__(self, noise_magnitude=0.2):
-        super(ConvModel, self).__init__()
+
+class ResModel(nn.Module):
+    """Model with residual/skip connection."""
+    def __init__(self, sub_module, skip_connection=None, noise_magnitude=0.2):
+        """
+
+        :param sub_module: model between input and skip connection.
+        :param skip_connection: operation to do in skip connection.
+        """
+        super(ResModel, self).__init__()
+
+        if not skip_connection:
+            self.skip_connection = lambda x, y: x + y
+        else:
+            self.skip_connection = skip_connection
 
         self._noise_magnitude = noise_magnitude
 
-        self._conv_part = nn.Sequential(
-            nn.Conv2d(1, 64, 3, padding=1),
-            nn.PReLU(),
-            nn.BatchNorm2d(64),
+        self._conv_part = sub_module
 
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.PReLU(),
-            nn.BatchNorm2d(64),
-
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.PReLU(),
-            nn.BatchNorm2d(64),
-
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.PReLU(),
-            nn.BatchNorm2d(64),
-
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.PReLU(),
-            nn.BatchNorm2d(64),
-
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.PReLU(),
-            nn.BatchNorm2d(64),
-
-            nn.Conv2d(64, 1, 3, padding=1),
-        )
-
-        self._tanh = nn.Tanh()
+        # self._tanh = nn.Tanh()
 
     def forward(self, x):
-        clean = x * (1 + self._tanh(self._conv_part(x)))
-        clean = self._tanh(clean)
+        clean = self.skip_connection(x, self._conv_part(x))
+        # clean = self._tanh(clean)
 
         return clean
+
+
+class BasicConv(nn.Module):
+    """Series of conv layers keeping the same image shape."""
+    def __init__(self, in_channels=1, n_layers=6):
+        super(BasicConv, self).__init__()
+        model = [nn.Sequential(nn.Conv2d(in_channels, 64, 3, padding=1),
+                               nn.PReLU(),
+                               nn.BatchNorm2d(64))
+                      ]
+        for _ in range(n_layers - 2):
+            model += [nn.Sequential(nn.Conv2d(64, 64, 3, padding=1),
+                                    nn.PReLU(),
+                                    nn.BatchNorm2d(64))
+                           ]
+        model += [nn.Conv2d(64, in_channels, 3, padding=1)]
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class LogAddDespeckle(nn.Module):
+    """Apply log to pixel values, resnet with addition, apply exponential."""
+    def __init__(self):
+        super(LogAddDespeckle, self).__init__()
+        conv = BasicConv(in_channels=1, n_layers=6)
+        self.remove_noise = ResModel(conv, skip_connection=lambda x, y: x + y)
+
+    def forward(self, x):
+        log_x = x.log()
+        clean_log_x = self.remove_noise(log_x)
+        return clean_log_x.exp()
