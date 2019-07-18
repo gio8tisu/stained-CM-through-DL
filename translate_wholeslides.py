87b141e73a1857b287843482b00d8ed980b2aae4
gio8tisu <gio8.tisu@gmail.com>
2019-07-19 00:01:56 +0200

    add normalization as slide transform

--- translate_wholeslides.py
+++ translate_wholeslides.py
@@ -14,7 +14,7 @@ from datasets import SkinCMDataset
 from utils import TileMosaic, pad_image
 
 
-pyvips.cache_set_max(0)
+pyvips.cache_set_max_mem(100)
 
 Image.MAX_IMAGE_PIXELS = None
 
@@ -38,8 +38,8 @@ def main(args, dataset, G_AB, transform, numpy2vips, cuda):
                 res_np = np.moveaxis(res_np, 1, 3)  # to channels last.
                 res_np = (res_np[0] + 1) / 2  # shift pixel values to [0,1] range
                 res = numpy2vips(res_np)  # convert to pyvips.Image
-                ver_image = res if not ver_image else ver_image.join(res, "vertical")  # "stack" vertically
-            image = ver_image if not image else image.join(ver_image, "horizontal")  # "stack" horizontally
+                ver_image = res if not ver_image else ver_image.join(res, 'vertical')  # "stack" vertically
+            image = ver_image if not image else image.join(ver_image, 'horizontal')  # "stack" horizontally
         if args.save_linear:
             save(args, i, image, scan)
         else:
@@ -112,19 +112,36 @@ def save(args, i, transformed, linear=None):
         print('Done.')
 
 
+def normalize(img):
+    """Normalize pyvips.Image by min and max.
+
+    Intended to be used with torch.transforms.Lambda
+    """
+    min_ = img.min()
+    range_ = min_ - img.max()
+    return (img - min_) / range_
+
+
+def scale(img, s=65535):
+    """Scale pyvips.Image by s.
+
+    Intended to be used with torch.transforms.Lambda
+    """
+    return img / s
+
+
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='Transform CM whole-slides to H&E.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('directory', type=str, help='directory with mosaic* directories')
+    parser.add_argument('data_directory', type=str, help='directory with mosaic* directories')
     parser.add_argument('--models-dir', required=True, help='directory with saved models')
     parser.add_argument('-o', '--output', required=True, help='output directory')
     parser.add_argument('--prefix', default='scan', help='output files prefix PREFIX')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--format', default='jpg', help='output image format')
-    group.add_argument('--compression', action='store_true',
-                       help='apply JPEG compression (with Q=90), assumes input images are in TIFF format.')
+    group.add_argument('--compression', action='store_true', help='apply JPEG compression (with Q=90).')
     parser.add_argument('--epoch', type=int, default=199, help='epoch to get model from.')
     parser.add_argument('--patch-size', type=int, default=2048, help='size in pixels of patch/window.')
     parser.add_argument('--crop-size', type=int,
@@ -140,6 +157,8 @@ if __name__ == '__main__':
     parser.add_argument('--window', default='rectangular',
                         help='window type for overlap option, should be and integer or one of: '
                              + 'rectangular, pyramid, circular or a number.')
+    parser.add_argument('--normalize', action='store_true',
+                        help='normalize slide (subtract min and divide by range)')
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('--debug', action='store_true')
 
@@ -165,23 +184,29 @@ if __name__ == '__main__':
     numpy2vips = numpy_pyvips.Numpy2Vips()
 
     # transform to apply patch-by-patch.
-    transform = transforms.Compose([numpy_pyvips.Vips2Numpy(),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                                    ])
+    patch_transform = transforms.Compose(
+        [numpy_pyvips.Vips2Numpy(),
+         transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+         ]
+    )
     # dataset of confocal large slides.
-    dataset = SkinCMDataset(args.directory, stain=True,
-                            transform_F=transforms.Lambda(lambda x: x / 65535),
-                            transform_R=transforms.Lambda(lambda x: x / 65535))
+    dataset = SkinCMDataset(
+        args.data_directory, stain=True,
+        transform_F=transforms.Lambda(normalize if args.normalize else scale),
+        transform_R=transforms.Lambda(normalize if args.normalize else scale)
+    )
 
     G_AB = cyclegan.models.GeneratorResNet(res_blocks=9)
     if cuda:
         G_AB = G_AB.cuda()
-    G_AB.load_state_dict(torch.load('%s/%s/G_AB_%d.pth' % (args.models_dir, args.dataset_name, args.epoch)))
+    G_AB.load_state_dict(torch.load(
+        os.path.join(args.models_dir, args.dataset_name, f'G_AB_{args.epoch}.pth')
+    ))
 
     G_AB.eval()
     with torch.no_grad():
         if args.overlap:
-            main_fancy(args, dataset, G_AB, transform, numpy2vips, cuda)
+            main_fancy(args, dataset, G_AB, patch_transform, numpy2vips, cuda)
         else:
-            main(args, dataset, G_AB, transform, numpy2vips, cuda)
+            main(args, dataset, G_AB, patch_transform, numpy2vips, cuda)
